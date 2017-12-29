a2dedfcf62aac21c6529106ab7469b52180f2250
Watermelon <watermelon8157@gmail.com>
2017-12-29 15:20:40 +0800

    新增  Algorithm_2 初級
    尚未完成

--- ai_algorithm.py
+++ ai_algorithm.py
@@ -1,7 +1,8 @@
 import random
+import pandas as pd
 from reversi import *
 
-
+		
 class Algorithm_1(object):
 
     def __init__(self, board, tile):
@@ -28,30 +29,22 @@ class Algorithm_1(object):
                 bestScore = score
         return bestMove
 
-
+source_csv = pd.read_csv('Algorithm_2.csv')
 class Algorithm_2(object):
 
     def __init__(self, board, tile):
         self.board = board
         self.tile = tile
-
+		
     def getBestMove(self):
         bestMove = None
-
         possibleMoves = getValidMoves(self.board, self.tile)
         random.shuffle(possibleMoves)
-
+        nowScore = -99
         for x, y in possibleMoves:
-            if isOnCorner(x, y):
-                return [x, y]
-
-        bestScore = -1
-        for x, y in possibleMoves:
-            dupeBoard = self.board
-            makeMove(dupeBoard, self.tile, x, y)
-            score = getScoreOfBoard(dupeBoard)[self.tile]
-            if score > bestScore:
-                bestMove = [x, y]
-                bestScore = score
+            source = source_csv[source_csv.columns[x]][y]
+            if source > nowScore:
+                nowScore = source
+                bestMove = [x,y]
+        #print(nowScore)
         return bestMove
-
